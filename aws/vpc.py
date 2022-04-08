f63ba20b2aa9e558085772e25bbd6c2634fecf0c
krishnamaram24 <91951316+krishnamaram24@users.noreply.github.com>
2022-04-09 01:07:51 +0530

    Update vpc.py

--- aws/vpc.py
+++ aws/vpc.py
@@ -1,7 +1,10 @@
 #https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html?msclkid=8a618619af8811ecbec36b227b035212
 
 import boto3
+import os
 
-client = boto3.client('ec2', region_name='us-west-2') # Adjust as desired
+accesskey = os.environ['AWS_ACCESS_KEY_ID']
+secretkey = os.environ['AWS_SECRET_ACCESS_KEY']
+client = boto3.client('ec2', region_name='us-west-2', aws_access_key_id='accesskey', aws_secret_access_key='secretkey') # Adjust as desired
 
 client.create_default_vpc()
