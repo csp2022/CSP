bc149b9d2bc92a7bae8aa22352b2422796437efd
krishnamaram24 <91951316+krishnamaram24@users.noreply.github.com>
2022-04-09 12:51:12 +0530

    Update vpc.py

--- aws/vpc.py
+++ aws/vpc.py
@@ -11,6 +11,6 @@ secretkey = str(sys.argv[2])
 print(accesskey, secretkey)
 
 
-ec2_resource = boto3.resource('ec2', region_name = 'us-west-2', aws_access_key_id = accesskey, aws_secret_access_key = secretkey)
+ec2_resource = boto3.client('ec2', region_name = 'us-west-2', aws_access_key_id = accesskey, aws_secret_access_key = secretkey)
 
 ec2_resource.create_default_vpc()
