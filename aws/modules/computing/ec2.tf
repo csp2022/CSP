f465e589ae02e1846c60e376a5415af6e71b408f
krishnamaram24 <91951316+krishnamaram24@users.noreply.github.com>
2022-04-30 22:19:55 +0530

    Update ec2.tf

--- aws/modules/computing/ec2.tf
+++ aws/modules/computing/ec2.tf
@@ -21,16 +21,3 @@ ebs_block_device {
   volume_size = "10"
   }
 }
-
-resource "aws_instance" "dbserver" {
-count = 1
-availability_zone = "us-east-1c"
-ami = "${var.myamiid}"
-instance_type = "t2.medium"
-subnet_id = "${var.publicsubnet}"
-private_ip= "192.168.1.7"
-vpc_security_group_ids = ["${var.websg}"]
-key_name = "${var.mykeypair}"
-user_data = "${var.userdata}"
-tags = "${merge(var.tags, map("Name", format("db-server-%d", count.index + 1)))}"
-}
