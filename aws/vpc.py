cac81439f3fe98c3e35b22dbb64e5c4cca21740c
Cloud User <centos@ip-172-31-11-156.us-west-2.compute.internal>
2022-04-30 16:18:55 +0000

    python boto3 code to provision infrastructure

--- aws/vpc.py
+++ aws/vpc.py
@@ -7,5 +7,7 @@ import sys
 accesskey = str(sys.argv[1])
 secretkey = str(sys.argv[2])
 
-ec2_resource = boto3.client('ec2', region_name = 'us-east-1', aws_access_key_id = accesskey, aws_secret_access_key = secretkey)
-ec2_resource.create_default_vpc()
+ec2_client = boto3.client('ec2', region_name = 'us-east-1', aws_access_key_id = accesskey, aws_secret_access_key = secretkey)
+vpc= ec2_client.create_vpc(CidrBlock='10.0.0.0/16') # create vpc
+print(vpc['Vpc']['VpcId'])
+#subnet= ec2_client.create_subnet(CidrBlock='10.0.1.0/24') # create subnet
