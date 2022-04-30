818c8e7be88059d0795692df055aad5131e183ef
krishnamaram24 <91951316+krishnamaram24@users.noreply.github.com>
2022-04-30 22:16:23 +0530

    Delete aws/modules/security/kms directory

--- aws/modules/security/kms/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "websg" {
-value = "${aws_security_group.websg.id}"
-}
-#output "appserver_publicip" {
-#value = "${aws_instance.appserver.public_ip}"
-#}
-#output "dbserver_publicip" {
-#value = "${aws_instance.dbserver.public_ip}"
-#}
